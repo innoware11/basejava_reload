9b568344fd57138bf3dad2c2f477f8e1f8a4dd9d
ichimax <chimax.prog@gmail.com>
2018-11-20 23:53:15 +0300

    HW1: implement ArrayStorage

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,28 +1,49 @@
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10_000];
+    int size;
 
     void save(Resume resume) {
+        storage[size] = resume;
+        size++;
     }
 
     Resume get(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                return storage[i];
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if(storage[i].uuid.equals(uuid)) {
+                size--;
+                storage[i] = storage[size];
+                storage[size] = null;
+            }
+        }
     }
 
     void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
+        }
+        size = 0;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] resumes = new Resume[size];
+        System.arraycopy(storage, 0, resumes, 0, size);
+        return resumes;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
